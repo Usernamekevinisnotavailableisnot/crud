0de5fe0b98e2a6446dd993b596edfb8c2827d4ba
azzolari.20233 <azzolari.20233@LABI2-13PC14>
2023-03-03 12:48:02 +0100

    Aggiungere i file di progetto.

---
+++ crud/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace crud
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listView4 = new System.Windows.Forms.ListView();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(224, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Aggiunta di un prodotto";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(564, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(224, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Eliminazione";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(776, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 76);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(377, 299);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(286, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(224, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "inserimento prezzo";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listView4
+            // 
+            this.listView4.HideSelection = false;
+            this.listView4.Location = new System.Drawing.Point(411, 404);
+            this.listView4.Name = "listView4";
+            this.listView4.Size = new System.Drawing.Size(377, 34);
+            this.listView4.TabIndex = 8;
+            this.listView4.UseCompatibleStateImageBehavior = false;
+            this.listView4.View = System.Windows.Forms.View.List;
+            this.listView4.SelectedIndexChanged += new System.EventHandler(this.listView4_SelectedIndexChanged);
+            // 
+            // listView2
+            // 
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(411, 76);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(377, 299);
+            this.listView2.TabIndex = 9;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.List;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.listView4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListView listView4;
+        private System.Windows.Forms.ListView listView2;
+    }
+}
+
---
+++ crud/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace crud
+{
+    
+    public partial class Form1 : Form
+    {
+        public string[] animali = new string[1000];
+        public int[] prezzo = new int[1000];
+        public int dim = 0;
+        public int dimen = 0;
+        public int posizione;
+        public string ca;
+        public int somma;
+        public int som = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Aggiun(animali,ref dim,textBox1.Text);
+            Stampa(textBox1.Text);
+        }
+      
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Eliminato(animali,ref dim, textBox1.Text);
+            
+        }
+
+       
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Stampo(textBox1.Text);
+            Prezzo(prezzo, ref dimen, textBox1.Text);
+        }
+
+        //funzioni di servizio
+        public void Aggiun(string[] animali,ref int dim, string ani)//funzione di Aggiunta
+        {
+            animali[dim] = ani;
+            dim++;
+        }
+        public void Prezzo(int[] prezzo, ref int dimen, string soldi)//funzione di Aggiunta
+        {
+            prezzo[dimen] = int.Parse(soldi);
+            dimen++;
+        }
+
+        public void Eliminato(string[] animali,ref int dim, string estinto)//funzione per eliminare un'animale
+        {
+
+            this.listView1.Items.Clear();
+            this.listView2.Items.Clear();
+            for (int i=posizione-1;i<dim; i++)
+            {
+                animali[i] = animali[i + 1];
+
+            }
+            dim--;
+            Stampa(ca);
+            
+        }
+        
+      
+        }
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        public void Stampa(string ani)//funzione stampa 
+        {
+            this.listView1.Items.Add(ani);
+           
+        }
+        public void Stampo(string soldi)//funzione stampa del prezzo
+        {
+            this.listView2.Items.Add(soldi);
+
+        }
+        
+        private void listView2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        public void somm(int[]prezzo,ref int dimen,ref int somma)
+        {   Sta(ref  somma);
+            for (int i=0;i<dimen;i++)
+            {
+                
+                somma = prezzo[i] + prezzo[i + 1];
+                i++;
+            }
+            
+        }
+        public void Sta(ref int somma)//funzione stampa del prezzo
+        {
+            this.listView4.Items.Add(somma.ToString());
+
+        }
+    }
+}
